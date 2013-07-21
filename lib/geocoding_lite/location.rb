3e8be6c35f63e54846f23efd62ad325d2651888d
Vladimir Ivic <vlada.ivic@gmail.com>
2013-07-21 15:17:38 -0700

    Update location.rb

--- lib/geocoding_lite/location.rb
+++ lib/geocoding_lite/location.rb
@@ -2,7 +2,7 @@ module GeocodingLite
   class Location
     attr_accessor :address, :latitude, :longitude
     
-    def map_response(response)
+    def map_response!(response)
       @location.address   = response[:address]
       @location.latitude  = response[:latitude]
       @location.longitude = response[:longitude]
